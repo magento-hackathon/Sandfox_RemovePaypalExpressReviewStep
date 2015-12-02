35e16606c0e37065e673ed9a0445b1e764466239
Simon Sprankel <simonsprankel@gmail.com>
2015-12-02 16:43:59 +0100

    fixed redirect issue

--- app/code/community/Sandfox/RemovePaypalExpressReviewStep/Model/Observer.php
+++ app/code/community/Sandfox/RemovePaypalExpressReviewStep/Model/Observer.php
@@ -6,4 +6,11 @@ class Sandfox_RemovePaypalExpressReviewStep_Model_Observer
 	{
 		Mage::app()->getResponse()->setRedirect(Mage::getUrl('*/*/placeOrder'));
 	}
+
+	public function controllerActionPredispatchPaypalExpressPlaceOrder(Varien_Event_Observer $observer)
+	{
+		$requiredAgreements = Mage::helper('checkout')->getRequiredAgreementIds();
+		$postedAgreements = array_fill_keys($requiredAgreements, 1);
+		Mage::app()->getRequest()->setPost('agreement', $postedAgreements);
+	}
 }
